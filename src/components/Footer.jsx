c9992eedbd803f5be0d3d9e28dfc1cf118d7c738
unknown <nilzak.indigo05@gmail.com>
2024-05-05 06:54:10 +0100

    fixed footer and open sidebar delay

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { LanguageContext } from '../contexts/LanguageContext';
 const Footer = () => {
   const { language } = useContext(LanguageContext);
-  return <footer className='bg-primary py-12'>
+  return <footer className='bg-primary py-8 fixed w-full z-10 bottom-0'>
     <div className="container mx-auto">
       <p className='text-white text-center'>
         {language === 'ar' ? '.جميع الحقوق محفوظة' : language === 'fr' ? 'Tous droits réservés.' : 'All rights reserved.'}
