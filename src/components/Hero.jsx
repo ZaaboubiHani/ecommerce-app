c89644043301bf9d032798cb20071c6b47b0bb13
unknown <nilzak.indigo05@gmail.com>
2024-07-10 16:44:23 +0100

    fixed heros page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,32 +1,55 @@
-import React, { useContext, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { LanguageContext } from '../contexts/LanguageContext';
-import { HeroContext } from '../contexts/HeroContext';
+import React, { useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { LanguageContext } from "../contexts/LanguageContext";
+import { HeroContext } from "../contexts/HeroContext";
+import ClipLoader from "react-spinners/ClipLoader";
+
 const Hero = () => {
   const { language } = useContext(LanguageContext);
-  const { fetchHeros,heros } = useContext(HeroContext);
+  const { fetchHeros, heros } = useContext(HeroContext);
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchHeros();
-  },[]);
-  return <section className='h-[800px] bg-hero bg-no-repeat bg-cover bg-center py-24'>
-    <div className='container mx-auto flex justify-around h-full transition-all duration-300'>
-      {/* text */}
-      <div className='flex flex-col justify-center'>
-        {/* pretitle */}
-        <div className='font-semibold flex items-center uppercase'>
-          <div className='w-10 h-[2px] bg-red-500 mr-3'></div>{language === 'ar' ? heros[0].arName: language === 'fr' ? heros[0].frName : heros[0].engName}
+  }, []);
+  return (
+    <section className="h-[800px] bg-hero bg-no-repeat bg-cover bg-center py-24">
+      {heros.length > 0 ? (
+        <div className="container mx-auto flex justify-around h-full transition-all duration-300">
+          {/* text */}
+          <div className="flex flex-col justify-center">
+            {/* pretitle */}
+            <div className="font-semibold flex items-center uppercase">
+              <div className="w-10 h-[2px] bg-red-500 mr-3"></div>
+              {language === "ar"
+                ? heros[0].arName
+                : language === "fr"
+                ? heros[0].frName
+                : heros[0].engName}
+            </div>
+            {/* title */}
+            <h1 className="text-[70px] leading-[1.1] font-light mb-4">
+              {language === "ar"
+                ? heros[1].arName
+                : language === "fr"
+                ? heros[1].frName
+                : heros[1].engName}
+              <br />
+            </h1>
+            <Link
+              to="/about"
+              className="self-start uppercase font-semibold border-b-2 border-primary"
+            >
+              {language === "ar"
+                ? heros[2].arName
+                : language === "fr"
+                ? heros[2].frName
+                : heros[2].engName}
+            </Link>
+          </div>
         </div>
-        {/* title */}
-        <h1 className='text-[70px] leading-[1.1] font-light mb-4'>{language === 'ar' ? heros[1].arName : language === 'fr' ? heros[1].frName : heros[1].engName}<br />
-          
-        </h1>
-        <Link to='/about' className='self-start uppercase font-semibold border-b-2 border-primary'>
-        {language === 'ar' ? heros[2].arName : language === 'fr' ? heros[2].frName : heros[2].engName}
-        </Link>
-      </div>
-    </div>
-  </section>;
+      ) : <ClipLoader />}
+    </section>
+  );
 };
 
 export default Hero;
